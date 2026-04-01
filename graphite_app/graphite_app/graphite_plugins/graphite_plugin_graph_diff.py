dc48d613f58dc744f1b1a9553c2db4116be72ce1
Matthew Wesney <157447210+dovvnloading@users.noreply.github.com>
2026-04-01 17:15:25 -0400

    Update graphite_plugin_graph_diff.py

--- graphite_app/graphite_app/graphite_plugins/graphite_plugin_graph_diff.py
+++ graphite_app/graphite_app/graphite_plugins/graphite_plugin_graph_diff.py
@@ -22,6 +22,7 @@ import graphite_config as config
 from graphite_canvas_items import HoverAnimationMixin
 from graphite_connections import ConnectionItem
 from graphite_config import get_current_palette, get_semantic_color
+from graphite_plugin_context_menu import PluginNodeContextMenu
 
 
 GRAPH_DIFF_SCROLLBAR_STYLE = """
@@ -869,6 +870,10 @@ class GraphDiffNode(QGraphicsObject, HoverAnimationMixin):
             if hasattr(self.scene(), "window"):
                 self.scene().window.setCurrentNode(self)
         super().mousePressEvent(event)
+
+    def contextMenuEvent(self, event):
+        menu = PluginNodeContextMenu(self)
+        menu.exec(event.screenPos())
 
     def mouseReleaseEvent(self, event):
         if self.scene():
